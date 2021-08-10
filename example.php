e9dffa0faf1677b931438c88bd36aa715ff6e1ae
SepehrSoheili <87861266+SepehrSoheili@users.noreply.github.com>
2021-08-10 17:05:42 +0400

    Create example.php
    create example.php

---
+++ example.php
@@ -0,0 +1,20 @@
+<?php
+
+require('telegramProxyCollector.php');
+
+$tpc = new telegramProxyCollector('@Username'); //username of channel
+
+
+$proxy1 = $tpc->getNewProxy(); //Get last proxy in channel
+
+$proxy2 = $tpc->getNewProxy(); //Get a proxy before the last
+
+// and etc ...
+
+
+$count = $tpc->getCount(); // Get count of all proxies that collector can collect
+
+$count = $tpc->getAll(); // Get all proxies that collector can collect
+
+
+

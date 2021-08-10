76632d285f290333a96b5deadd91b725ff32bf95
SepehrSoheili <87861266+SepehrSoheili@users.noreply.github.com>
2021-08-10 16:53:33 +0400

    Create telegramProxyCollector.php
    add telegramProxyCollector.php

---
+++ telegramProxyCollector.php
@@ -0,0 +1,49 @@
+<?php
+class telegramProxyCollector{
+    private $data;
+	private $pointer = 0;
+    public function __construct($channelUsername){
+	
+	
+	    $channelUsername = str_replace('@','',$channelUsername);
+	
+	    $this->data = file_get_contents('https://t.me/s/'.$channelUsername);
+	    if($this->data === false)
+	        throw new Exception('Your host or server can`t connect to telegram servers');
+	
+	    preg_match_all('#(?<=href=")https:\/\/t(?:elegram)?\.me\/proxy\?server[^"]+#i',$this->data,$this->data);
+	    
+	    $this->data = array_unique(array_reverse($this->data[0]));
+
+	
+		
+
+
+
+	}
+
+
+	// Get new proxy from channel, If the proxy ends, getNewproxy() returns false
+	public function getNewProxy(){
+		if(count($this->data) > $this->pointer){
+		$proxy = $this->data[$this->pointer];
+		$this->pointer  += 1;
+		return $proxy;			
+		}else
+		return false;
+	}
+
+	//Get all Proxies
+    public function getAll(){
+    	
+    	return $this->data;
+    }
+	//Get proxies count
+	public function getCount(){
+	
+		return count($this->data);
+	}
+
+
+
+}
